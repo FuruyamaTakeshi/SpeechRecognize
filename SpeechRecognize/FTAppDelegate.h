90620d24c39bddd32b29f33a6e2b725b4c92078b
FuruyamaTakeshi <rx13b654x2@gmail.com>
2013-11-04 14:02:42 +0900

    Initial Commit

---
+++ SpeechRecognize/FTAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  FTAppDelegate.h
+//  SpeechRecognize
+//
+//  Created by 古山 健司 on 2013/11/04.
+//  Copyright (c) 2013年 TF. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FTAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
